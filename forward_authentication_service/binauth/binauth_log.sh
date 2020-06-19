aa04961f8b9eadadd8b901bd3ecbb76ba40ec38f
Rob White <rob@blue-wave.net>
2020-06-19 19:38:05 +0100

    Documentation updates - fas and binauth
    Signed-off-by: Rob White <rob@blue-wave.net>

--- forward_authentication_service/binauth/binauth_log.sh
+++ forward_authentication_service/binauth/binauth_log.sh
@@ -85,12 +85,14 @@ write_log () {
 # Possible values are:
 # "auth_client" - NDS requests validation of the client
 # "client_auth" - NDS has authorised the client
-# "client_deauth" - NDS has deauthorised the client
-# "idle_deauth" - NDS has deauthorised the client because the idle timeout duration has been exceeded
-# "timeout_deauth" - NDS has deauthorised the client because the session length duration has been exceeded
+# "client_deauth" - NDS has deauthenticated the client
+# "idle_deauth" - NDS has deauthenticated the client because the idle timeout duration has been exceeded
+# "timeout_deauth" - NDS has deauthenticated the client because the session length duration has been exceeded
+# "downquota_deauth" - NDS has deauthenticated the client because the client's download quota has been exceeded
+# "upquota_deauth" - NDS has deauthenticated the client because the client's upload quota has been exceeded
 # "ndsctl_auth" - NDS has authorised the client because of an ndsctl command
-# "ndsctl_deauth" - NDS has deauthorised the client because of an ndsctl command
-# "shutdown_deauth" - NDS has deauthorised the client because it received a shutdown command
+# "ndsctl_deauth" - NDS has deauthenticated the client because of an ndsctl command
+# "shutdown_deauth" - NDS has deauthenticated the client because it received a shutdown command
 #
 action=$1
 

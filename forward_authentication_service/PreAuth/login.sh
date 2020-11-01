eaceace105b76638d52185da93ec2b4b561e7d16
Rob White <rob@blue-wave.net>
2020-11-01 14:06:33 +0000

    login.sh: rework to support login mode
    Signed-off-by: Rob White <rob@blue-wave.net>

--- forward_authentication_service/PreAuth/login.sh
+++ forward_authentication_service/PreAuth/login.sh
@@ -7,40 +7,129 @@
 # This is changed to bash automatically by Makefile for Debian
 #
 
-
-# Customise the Logfile location:
-#
-# mountpoint is the mount point for the storage the log is to be kept on
-#
-# /tmp on OpenWrt is tmpfs (ram disk) and does not survive a reboot.
-#
-# /run on Raspbian is also tmpfs and also does not survive a reboot.
-#
-# These choices for OpenWrt and Raspbian are a good default for testing purposes
-# as long term use on internal flash could cause memory wear
-# In a production system, use the mount point of a usb drive for example
-#
-#
-# logdir is the directory path for the log file
-#
-#
-# logname is the name of the log file
-#
-
-#For Openwrt:
-mountpoint="/tmp"
-logdir="/tmp/ndslog/"
-logname="ndslog.log"
-
-#For Raspbian:
-#mountpoint="/run"
-#logdir="/run/ndslog/"
-#logname="ndslog.log"
-
-#For logging
-ndspid=$(ps | grep '/usr/bin/opennds' | awk -F ' ' 'NR==2 {print $1}')
+# In this example script we want to either ask the client user for
+# their username and email address or to give them a click to continue button.
+# See end of file for a programming guide.
 
 # functions:
+
+thankyou_page () {
+	# If we got here, we have both the username and emailaddr fields as completed on the login page on the client,
+	# or Continue has been clicked on the "Click to Continue" page
+	# so we will now call ndsctl to get client data we need to authenticate and add to our log.
+
+	# Variables returned from ndsctl are listed in $varlist.
+
+	# We at least need the client token to authenticate.
+	# In this example we will also log the client mac address.
+
+	varlist="id ip mac added active duration token state downloaded avg_down_speed uploaded avg_up_speed"
+	clientinfo=$(ndsctl json $clientip)
+
+	if [ -z "$clientinfo" ]; then
+		echo "<big-red>Sorry!</big-red><italic-black> The portal is busy, please try again.</italic-black><hr>"
+		echo -e "$continue_form"
+		echo -e "$footer"
+		exit 0
+	else
+		# Populate varlist with client data:
+		for var in $varlist; do
+			eval $var=$(echo "$clientinfo" | grep $var | awk -F'"' '{print $4}')
+		done
+	fi
+
+	# For openNDS auth we need from varlist:
+	tok=$token
+	clientmac=$mac
+
+	# You can choose to send a custom data string to BinAuth. Set the variable $custom to the desired value
+	# OR uncomment the text input in the displayed form to get input from the client
+	# Max length 256 characters
+
+	custom=""
+
+	# We now output the "Thankyou page" with a "Continue" button.
+
+	# This is the place to include information or advertising on this page,
+	# as this page will stay open until the client user taps or clicks "Continue"
+
+	# Be aware that many devices will close the login browser as soon as
+	# the client user continues, so now is the time to deliver your message.
+
+	echo "<big-red>Thankyou for using this service!</big-red>"
+	echo "<br><b>Welcome $usernamehtml</b>"
+
+	# Add your message here:
+	# You could retrieve text or images from a remote server using wget or curl
+	# as this router has Internet access whilst the client device does not (yet).
+	echo "<br><italic-black> Your News or Advertising could be here, contact the owners of this Hotspot to find out how!</italic-black>"
+
+	echo "<form action=\"/opennds_auth/\" method=\"get\">"
+	echo "<input type=\"hidden\" name=\"tok\" value=\"$tok\">"
+	echo "<input type=\"hidden\" name=\"redir\" value=\"$requested\"><br>"
+
+	#uncomment the next line to request a custom string input from the client and forward it to BinAuth
+	#echo "<input type=\"text\" name=\"custom\" value=\"$custom\" required><br>Custom Data<br><br>"
+
+	# or uncomment the next line to forward a pre defined variable %custom to BinAuth
+	#echo "<input type=\"hidden\" name=\"custom\" value=\"$custom\""
+
+	echo "<input type=\"submit\" value=\"Continue\" >"
+	echo "</form><hr>"
+
+	# In this example we have decided to log all clients who are granted access
+	write_log
+
+}
+
+name_email_login() {
+	# For this simple example, we check that both the username and email address fields have been filled in.
+	# If not then serve the initial page, again if necessary.
+	# We are not doing any specific validation in this example, but here is the place to do it if you need to.
+	#
+	# Note if only one of username or email address fields is entered then that value will be preserved
+	# and displayed on the page when it is re-served.
+	#
+	# Note also $clientip, $gatewayname and $requested (redir) must always be preserved
+	#
+
+	form="$login_form"
+
+	if [ ! -z "$username" ] && [ ! -z "$emailaddr" ]; then
+		thankyou_page
+		echo -e "$footer"
+		exit 0
+	fi
+
+	echo "<big-red>Welcome!</big-red><br>
+		<med-blue>You are connected to $client_zone</med-blue><br>
+		<italic-black>To access the Internet you must enter your full name and email address then click Continue to proceed.</italic-black><hr>"
+	echo -e "$form"
+
+	echo -e "$footer"
+	exit 0
+}
+
+click_to_continue() {
+	# Note $clientip, $gatewayname and $requested (redir) must always be preserved
+	#
+
+	form="$continue_form"
+
+	if [ "$continue" = "clicked" ]; then
+		thankyou_page
+		echo -e "$footer"
+		exit 0
+	fi
+
+	echo "<big-red>Welcome!</big-red><br>
+		<med-blue>You are connected to $client_zone</med-blue><br>
+		<italic-black>To access the Internet you must click Continue to proceed.</italic-black><hr>"
+	echo -e "$form"
+
+	echo -e "$footer"
+	exit 0
+}
 
 htmlentityencode() {
 	entitylist="s/\"/\&quot;/ s/>/\&gt;/ s/</\&lt;/"
@@ -107,9 +196,25 @@ write_log () {
 	fi
 }
 
-# Get the urlencoded querystring and user_agent
+
+# Customise the Logfile location:
+#For Openwrt:
+mountpoint="/tmp"
+logdir="/tmp/ndslog/"
+logname="ndslog.log"
+
+#For Raspbian:
+#mountpoint="/run"
+#logdir="/run/ndslog/"
+#logname="ndslog.log"
+
+#For syslog
+ndspid=$(pgrep '/usr/bin/opennds')
+
+# Get the urlencoded querystring, user_agent and mode
 query_enc=$(echo "$1" | sed "s/%3f/%20/")
 user_agent_enc="$2"
+mode="$3"
 
 # The query string is sent to us from NDS in a urlencoded form,
 # we can decode it or parts of it using something like the following:
@@ -117,55 +222,6 @@ user_agent_enc="$2"
 
 # The User Agent string is sent urlencoded also:
 user_agent=$(printf "${user_agent_enc//%/\\x}")
-
-# In this example script we want to ask the client user for
-# their username and email address.
-#
-# We could ask for anything we like and add our own variables to the html forms
-# we generate.
-#
-# If we want to show a sequence of forms or information pages we can do this easily.
-#
-# To return to this script and show additional pages, the form action must be set to:
-#	<form action=\"/opennds_preauth/\" method=\"get\">
-# Note: quotes ( " ) must be escaped with the "\" character.
-#
-# Any variables we need to preserve and pass back to ourselves or NDS must be added 
-# to the form as hidden:
-#	<input type=\"hidden\" name=......
-# Such variables will appear in the query string when NDS re-calls this script.
-# We can then parse for them again.
-#
-# When the logic of this script decides we should allow the client to access the Internet
-# we inform NDS with a final page displaying a continue button with the form action set to:
-#	"<form action=\"/opennds_auth/\" method=\"get\">"
-#
-# We must also send NDS the client token as a hidden variable, but first we must obtain
-# the token from ndsctl using a suitable command such as:
-#	tok="$(ndsctl json $clientip | grep token | cut -c 10- | cut -c -8)"
-#
-# In a similar manner we can obtain any client or NDS information that ndsctl provides. 
-
-# The query string NDS sends to us will always be of the following form (with a "comma space" separator):
-# ?clientip=[clientipaddress], gatewayname=[gatewayname], redir=[originalurl], var4=[data], var5=[data], var6......
-#
-# The first three variables will be clientip, gatewayname and redir
-#
-# We have chosen to name redir as $requested here as it is actually the originally requested url.
-#
-# There is one exception to this. If the client presses "back" on their browser NDS detects this
-# and tells us by returning status=authenticated instead of redir=[originalurl]
-# If we detect this we show a page telling the client they are already logged in.
-#
-# Additional variables returned by NDS will be those we define here and send to NDS via an
-# html form method=get
-# See the examples here for $username and $emailaddress
-#
-# There is no limit to the number of variables we can define dynamically
-# as long as the query string does not exceed 2048 bytes.
-#
-# The query string will be truncated if it does exceed this length.
-
 
 # Parse for the variables returned by NDS:
 hid_present=$(echo "$query_enc" | grep "hid")
@@ -178,9 +234,10 @@ elif [ -z "$hid_present" ]; then
 	gatewayaddress="0"
 	queryvarlist="clientip gatewayname redir username emailaddr"
 else
-	queryvarlist="clientip gatewayname hid gatewayaddress redir username emailaddr"
+	queryvarlist="clientip gatewayname hid gatewayaddress redir username emailaddr continue"
 fi
 
+#Populate querylist variables from data in the query string
 for var in $queryvarlist; do
 	nextvar=$(echo "$queryvarlist" | awk '{for(i=1;i<=NF;i++) if ($i=="'$var'") printf $(i+1)}')
 	eval $var=$(echo "$query_enc" | awk -F "%20$var%3d" '{print $2}' | awk -F "%2c%20$nextvar%3d" '{print $1}')
@@ -204,27 +261,11 @@ requested=$(printf "${redir//%/\\x}" | awk -F ', ' '{print $1}')
 #Get the client zone, local wired, local wireless or remote mesh node
 get_client_zone
 
-# Define some common html as the first part of the page to be served by NDS
-#
-# Note this example uses the default splash.css provided by NDS and uses splash.jpg
-# as the browser shortcut icon.
-#
-# You can decide how your PreAuth splash page will look
-# by incorporating your own css and images.
-#
-# Note however that the output of this script will be displayed on the client device screen via the CPD process on that device.
-# It should be noted when designing a custom splash page that for security reasons many client device CPD implementations:
-#
-#	Immediately close the browser when the client has authenticated.
-#	Prohibit the use of href links.
-#	Prohibit downloading of external files (including .css and .js, even if they are allowed in NDS firewall settings).
-#	Prohibit the execution of javascript.
-#
-
 if [ "$status" = "authenticated" ]; then
 	gatewaynamehtml="Welcome"
 fi
 
+# Define a common header html for every page served
 header="<!DOCTYPE html>
 	<html>
 	<head>
@@ -244,7 +285,7 @@ header="<!DOCTYPE html>
 	<hr>
 "
 
-# Define a common footer for every page served
+# Define a common footer html for every page served
 version="$(ndsctl status | grep Version)"
 year="$(date | awk -F ' ' '{print $(6)}')"
 footer="
@@ -274,10 +315,25 @@ login_form="
 	</form><hr>
 "
 
+# Define a click to Continue form
+continue_form="
+	<form action=\"/opennds_preauth/\" method=\"get\">
+	<input type=\"hidden\" name=\"clientip\" value=\"$clientip\">
+	<input type=\"hidden\" name=\"gatewayname\" value=\"$gatewaynamehtml\">
+	<input type=\"hidden\" name=\"hid\" value=\"$hid\">
+	<input type=\"hidden\" name=\"gatewayaddress\" value=\"$gatewayaddress\">
+	<input type=\"hidden\" name=\"redir\" value=\"$requested\">
+	<input type=\"hidden\" name=\"continue\" value=\"clicked\">
+	<input type=\"submit\" value=\"Continue\" >
+	</form><hr>
+"
+
+# Serve the Portal "Splash" pages:
+
 # Output the page common header
 echo -e "$header"
 
-# Check if the client is already logged in and has tapped "back" on their browser
+# Check if the client is already logged in (have probably tapped "back" on their browser)
 # Make this a friendly message explaining they are good to go
 if [ "$status" = "authenticated" ]; then
 	echo "<p><big-red>You are already logged in and have access to the Internet.</big-red></p>"
@@ -287,89 +343,94 @@ if [ "$status" = "authenticated" ]; then
 	exit 0
 fi
 
-# For this simple example, we check that both the username and email address fields have been filled in.
-# If not then serve the initial page, again if necessary.
-# We are not doing any specific validation in this example, but here is the place to do it if you need to.
-#
-# Note if only one of username or email address fields is entered then that value will be preserved
-# and displayed on the page when it is re-served.
-#
-# Note also $clientip, $gatewayname and $requested (redir) must always be preserved
-#
-if [ -z "$username" ] || [ -z "$emailaddr" ]; then
+# Check login mode and serve appropriate form
+if [ $mode -eq 0 ]; then
+	click_to_continue
 
-	echo "<big-red>Welcome!</big-red><br>
-		<med-blue>You are connected to $client_zone</med-blue><br>
-		<italic-black>To access the Internet you must enter your Name and Email Address</italic-black><hr>"
-	echo -e "$login_form"
+elif [ $mode -eq 1 ]; then
+	name_email_login
+
 else
-	# If we got here, we have both the username and emailaddr fields as completed on the login page on the client,
-	# so we will now call ndsctl to get client data we need to authenticate and add to our log.
-
-	# Variables returned from ndsctl are listed in $varlist.
-
-	# We at least need the client token to authenticate.
-	# In this example we will also log the client mac address.
-
-	varlist="id ip mac added active duration token state downloaded avg_down_speed uploaded avg_up_speed"
-	clientinfo=$(ndsctl json $clientip)
-
-	if [ -z "$clientinfo" ]; then
-		echo "<big-red>Sorry!</big-red><italic-black> The portal is busy, please try again.</italic-black><hr>"
-		echo -e "$login_form"
-		echo -e "$footer"
-		exit 0
-	else
-		for var in $varlist; do
-			eval $var=$(echo "$clientinfo" | grep $var | awk -F'"' '{print $4}')
-		done
-	fi
-
-	tok=$token
-	clientmac=$mac
-
-	# We now output the "Thankyou page" with a "Continue" button.
-
-	# This is the place to include information or advertising on this page,
-	# as this page will stay open until the client user taps or clicks "Continue"
-
-	# Be aware that many devices will close the login browser as soon as
-	# the client user continues, so now is the time to deliver your message.
-
-	echo "<big-red>Thankyou!</big-red>"
-	echo "<br><b>Welcome $usernamehtml</b>"
-
-	# Add your message here:
-	# You could retrieve text or images from a remote server using wget or curl
-	# as this router has Internet access whilst the client device does not (yet).
-
-	# You can also send a custom data string to BinAuth. Set the variable $custom to the desired value
-	# or uncomment the text input in the displayed form to get input from the client
-	# Max length 256 characters
-	custom=""
-
-	echo "<br><italic-black> Your News or Advertising could be here, contact the owners of this Hotspot to find out how!</italic-black>"
-
-	echo "<form action=\"/opennds_auth/\" method=\"get\">"
-	echo "<input type=\"hidden\" name=\"tok\" value=\"$tok\">"
-	echo "<input type=\"hidden\" name=\"redir\" value=\"$requested\"><br>"
-
-	#uncomment the next line to request a custom string input from the client and forward it to BinAuth
-	#echo "<input type=\"text\" name=\"custom\" value=\"$custom\" required><br>Custom Data<br><br>"
-
-	# or uncomment the next line to forward the variable %custom to BinAuth
-	#echo "<input type=\"hidden\" name=\"custom\" value=\"$custom\""
-
-	echo "<input type=\"submit\" value=\"Continue\" >"
-	echo "</form><hr>"
-
-	# In this example we have decided to log all clients who are granted access
-	write_log
+	echo "<b>Invalid login mode #$mode</b>"
+	echo -e "$footer"
 fi
 
-# Output the page footer
-echo -e "$footer"
+
 # The output of this script could of course be much more complex and
 # could easily be used to conduct a dialogue with the client user.
 #
 
+
+# Programing Guide:
+#
+# We could ask for anything we like and add our own variables to the html forms
+# we generate.
+#
+# If we want to show a sequence of forms or information pages we can do this easily.
+#
+# To return to this script and show additional pages, the form action must be set to:
+#	<form action=\"/opennds_preauth/\" method=\"get\">
+# Note: quotes ( " ) must be escaped with the "\" character.
+#
+# Any variables we need to preserve and pass back to ourselves or NDS must be added
+# to the form as hidden:
+#	<input type=\"hidden\" name=......
+# Such variables will appear in the query string when NDS re-calls this script.
+# We can then parse for them again.
+#
+# When the logic of this script decides we should allow the client to access the Internet
+# we inform NDS with a final page displaying a continue button with the form action set to:
+#	"<form action=\"/opennds_auth/\" method=\"get\">"
+#
+# We must also send NDS the client token as a hidden variable, but first we must obtain
+# the token from ndsctl using a suitable command such as:
+#	tok="$(ndsctl json $clientip | grep token | cut -c 10- | cut -c -8)"
+#
+# In a similar manner we can obtain any client or NDS information that ndsctl provides.
+
+# The query string NDS sends to us will always be of the following form (with a "comma space" separator):
+# ?clientip=[clientipaddress], gatewayname=[gatewayname], redir=[originalurl], var4=[data], var5=[data], var6......
+#
+# The first three variables will be clientip, gatewayname and redir
+#
+# We have chosen to name redir as $requested here as it is actually the originally requested url.
+#
+# There is one exception to this. If the client presses "back" on their browser NDS detects this
+# and tells us by returning status=authenticated instead of redir=[originalurl]
+# If we detect this we show a page telling the client they are already logged in.
+#
+# Additional variables returned by NDS will be those we define here and send to NDS via an
+# html form method=get
+# See the examples here for $username and $emailaddress
+#
+# There is no limit to the number of variables we can define dynamically
+# as long as the query string does not exceed 2048 bytes.
+#
+# The query string will be truncated if it does exceed this length.
+#
+#
+# Customise the Logfile location:
+# mountpoint is the mount point for the storage the log is to be kept on
+# /tmp on OpenWrt is tmpfs (ram disk) and does not survive a reboot.
+# /run on Raspbian is also tmpfs and also does not survive a reboot.
+#
+# These choices for OpenWrt and Raspbian are a good default for testing purposes
+# as long term use on internal flash could cause memory wear
+# In a production system, use the mount point of a usb drive for example
+#
+# logdir is the directory path for the log file
+# logname is the name of the log file
+# Note this example uses the default splash.css provided by NDS and uses splash.jpg
+# as the browser shortcut icon.
+#
+# You can decide how your PreAuth splash page will look
+# by incorporating your own css and images.
+#
+# Note however that the output of this script will be displayed on the client device screen via the CPD process on that device.
+# It should be noted when designing a custom splash page that for security reasons many client device CPD implementations:
+#
+#	Immediately close the browser when the client has authenticated.
+#	Prohibit the use of href links.
+#	Prohibit downloading of external files (including .css and .js, even if they are allowed in NDS firewall settings).
+#	Prohibit the execution of javascript.
+#

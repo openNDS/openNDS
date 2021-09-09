83cb2f7a3d8376ed8dce2e49985ac5f102d0f8fe
Rob White <rob@blue-wave.net>
2021-09-09 23:25:13 +0100

    Add - Dnsmasq RFC8910 configuration
    Signed-off-by: Rob White <rob@blue-wave.net>

--- forward_authentication_service/libs/dnsconfig.sh
+++ forward_authentication_service/libs/dnsconfig.sh
@@ -6,6 +6,29 @@
 # This is changed to bash automatically by Makefile for generic Linux
 #
 
+# Define the dnsmask config file and hosts file locations for generic Linux
+# Edit these if your system uses a non standard locations:
+conflocation="/etc/dnsmasq.conf"
+hosts="/etc/hosts"
+#
+
+setconf="$1"
+uciconfig=$(uci show dhcp 2>/dev/null)
+
+delete_114s() {
+
+	if [ ! -z "$cpidconfig" ]; then
+
+		for option114 in $cpidconfig; do
+			is_114=$(echo "$option114" | grep "114")
+
+			if [ ! -z "$is_114" ]; then
+				echo "$dellist'$option114'" | uci batch
+			fi
+		done
+	fi
+}
+
 restart_dnsmasq() {
 	if [ "$uciconfig" = "" ]; then
 		systemctl restart dnsmasq &
@@ -14,19 +37,17 @@ restart_dnsmasq() {
 	fi
 }
 
-setconf="$1"
-uciconfig=$(uci show dhcp 2>/dev/null)
-
 if [ "$setconf" = "" ]; then
 	exit 1
+
 elif [ "$setconf" = "restart_only" ]; then
 	restart_dnsmasq
 	exit 0
+
 elif [ "$setconf" = "ipsetconf" ]; then
 	ipsetconf=$2	
 
 	if [ "$uciconfig" = "" ]; then
-		conflocation="/etc/dnsmasq.conf"
 		sed -i '/System\|walledgarden/d' $conflocation
 		echo "ipset=$ipsetconf" >> $conflocation
 	else
@@ -39,12 +60,13 @@ elif [ "$setconf" = "ipsetconf" ]; then
 		fi
 		uci add_list dhcp.@dnsmasq[0].ipset=$ipsetconf
 	fi
+
 	exit 0
+
 elif [ "$setconf" = "hostconf" ]; then
 	gw_ip=$2
 	gw_fqdn=$3
 	host_entry="$gw_ip $gw_fqdn " 
-	hosts="/etc/hosts"
 	host_entry_check=$(grep "$host_entry" "$hosts")
 	ip_check=$(grep "$gw_ip" "$hosts" | awk 'NR==1{printf("%s ", $1)}')
 	fqdn_check=$(grep "$gw_fqdn" "$hosts" | awk 'NR==1{printf("%s ", $2)}')
@@ -66,6 +88,43 @@ elif [ "$setconf" = "hostconf" ]; then
 	fi
 
 	exit 0
+
+elif [ "$setconf" = "cpidconf" ]; then
+	gatewayfqdn=$2
+
+	if [ "$uciconfig" = "" ]; then
+		# Generic Linux
+		sed -i '/System\|lan,114,http:/d' $conflocation
+
+		if [ ! -z "$gatewayfqdn" ]; then
+			echo "dhcp-option-force=lan,114,http://$gatewayfqdn" >> $conflocation
+		fi
+	else
+		# OpenWrt
+		cpidconfig=$(uci get dhcp.lan.dhcp_option_force 2>/dev/null)
+		dellist="del_list dhcp.lan.dhcp_option_force="
+
+		if [ -z "$gatewayfqdn" ]; then
+			delete_114s
+			uci commit dhcp
+			exit 0
+		fi
+
+		addlist="add_list dhcp.lan.dhcp_option_force='114,http://$gatewayfqdn'"
+
+		if [ -z "$cpidconfig" ]; then
+			echo $addlist | uci batch
+			uci commit dhcp
+
+		elif [ "$cpidconfig" != "114,http://$gatewayfqdn" ]; then
+			delete_114s
+			echo $addlist | uci batch
+			uci commit dhcp
+		fi
+	fi
+
+	exit 0
+
 else
 	exit 1 
 fi

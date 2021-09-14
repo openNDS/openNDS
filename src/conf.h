5d11e937de2f9409b824c25d28ba3704f0143dd2
Rob White <rob@blue-wave.net>
2021-09-14 21:09:59 +0100

    Bump to v9.4.0beta
    Signed-off-by: Rob White <rob@blue-wave.net>

--- src/conf.h
+++ src/conf.h
@@ -29,7 +29,7 @@
 #ifndef _CONF_H_
 #define _CONF_H_
 
-#define VERSION "9.3.1beta"
+#define VERSION "9.4.0beta"
 
 /*
  * Defines how many times should we try detecting the interface with the default route (in seconds).

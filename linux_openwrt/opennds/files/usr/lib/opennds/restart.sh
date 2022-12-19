8bf802a6746d259cc5da59aad74871d0d636e7fa
Rob White <rob@blue-wave.net>
2022-12-19 22:36:28 +0000

    Add - Change fwhook to add users to router rule to fw4 on OpenWrt
    Signed-off-by: Rob White <rob@blue-wave.net>

--- linux_openwrt/opennds/files/usr/lib/opennds/restart.sh
+++ linux_openwrt/opennds/files/usr/lib/opennds/restart.sh
@@ -3,9 +3,20 @@
 # Check if opennds is running
 ndspid=$(pidof opennds)
 fwhook=$(uci -q get opennds.@opennds[0].fwhook_enabled)
+
 if [ ! -z $ndspid ]; then
-  if [ "$fwhook" = "1" ]; then
-    echo "fwhook restart request received - restarting " | logger -p "daemon.warn" -s -t "opennds[$ndspid]: "
-    /etc/init.d/opennds restart
-  fi
+	if [ "$fwhook" = "1" ]; then
+
+		/usr/lib/opennds/libopennds users_to_router allow
+
+		ret=$?
+
+		if [ $ret -eq 0 ]; then
+			echo "fwhook signalled thefirewall is restarting, so adding rule to chain $inputchain for interface $gatewayinterface  " \
+				| logger -p "daemon.info" -s -t "opennds[$ndspid]"
+		else
+			echo "fwhook signalled thefirewall is restarting, but error $ret occured adding rule to chain $inputchain for interface $gatewayinterface  " \
+				| logger -p "daemon.error" -s -t "opennds[$ndspid]"
+		fi
+	fi
 fi

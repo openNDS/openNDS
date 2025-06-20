1811281b0a58c30a62b2abe9cfbd3d6a6543a3fa
Rob White <rob@blue-wave.net>
2025-06-20 08:12:15 +0100

    Bump to version 10.4.0beta
    Signed-off-by: Rob White <rob@blue-wave.net>

--- src/conf.h
+++ src/conf.h
@@ -30,7 +30,7 @@
 #ifndef _CONF_H_
 #define _CONF_H_
 
-#define VERSION "10.3.2beta"
+#define VERSION "10.4.0beta"
 
 /*
  * Defines how many times should we try detecting the interface with the default route (in seconds).
